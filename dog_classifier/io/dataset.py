ac510c4637f30c44d4d450d521656f33b1b49810
beckstev <beckstev@web.de>
2019-06-09 19:07:11 +0200

    Commented the other functions

--- dog_classifier/io/dataset.py
+++ dog_classifier/io/dataset.py
@@ -30,7 +30,7 @@ def generate_dataset(path_to_labels, train_size, val_size, test_size, inital_run
                            generate dataset
     '''
 
-    if inital_run ==  True:
+    if inital_run is True:
         rename_label_files(path_to_labels)
 
     # list which will include the label of every image
@@ -46,7 +46,6 @@ def generate_dataset(path_to_labels, train_size, val_size, test_size, inital_run
         # Each directories include a variety of label files (.xml) which
         # includes the dog race and bbox of every image.
         for label_file in os.listdir(path_to_dog_race_labels):
-
 
             path_to_image_label = os.path.join(path_to_dog_race_labels,
                                                label_file)
@@ -82,8 +81,8 @@ def generate_dataset(path_to_labels, train_size, val_size, test_size, inital_run
 
                 except TypeError:
                     race_label = doc['annotation']['object'][0]['name']
-                    # The xmin/xmax and ymin/xmax values of all bounding boxes will
-                    # be saved to the following lists
+                    # The xmin/xmax and ymin/xmax values of all bounding boxes
+                    # will be saved to the following lists
                     x_cor = []
                     y_cor = []
 
@@ -125,16 +124,15 @@ def generate_dataset(path_to_labels, train_size, val_size, test_size, inital_run
                 # all the labels
                 labels.append(dog_labels)
 
-
     columns = ["x1", "y1", "x2", "y2", "x3", "y3", "x4", "y4", "race_label",
                "width", "height", "path_to_label", "filename"]
     # Save all the labels to a Dataframe
-    df_label= pd.DataFrame(data=labels, columns=columns, dtype='uint16')
+    df_label = pd.DataFrame(data=labels, columns=columns, dtype='uint16')
 
     df_train_val, df_test = train_test_split(df_label, train_size=train_size + val_size,
-                                           test_size=test_size,
-                                           shuffle=True,
-                                           random_state=13)
+                                             test_size=test_size,
+                                             shuffle=True,
+                                             random_state=13)
 
     # We need to normalize the percentage amount of train and val, because
     # the parts train_size and val_size are for the whole dataset and not for
@@ -144,9 +142,9 @@ def generate_dataset(path_to_labels, train_size, val_size, test_size, inital_run
     val_new = val_size + norm
 
     df_train, df_val = train_test_split(df_train_val, train_size=train_new,
-                                      test_size=val_new,
-                                      shuffle=True,
-                                      random_state=13)
+                                        test_size=val_new,
+                                        shuffle=True,
+                                        random_state=13)
 
     save_path = os.path.join(Path(os.path.abspath(__file__)).parents[2],
                              "labels/")
@@ -160,6 +158,18 @@ def generate_dataset(path_to_labels, train_size, val_size, test_size, inital_run
 
 
 def sort_corners(corners):
+    ''' To make sure that we have the same coordinates order for
+        every bbox we use the function sort_corners. The function
+        sorts into the following order:
+
+        (x1, y1): top left corner
+        (x2, y2): top right corner
+        (x3, y3): bottom right corner
+        (x4, y4): bottom left corner
+
+        :param corners: Numpy array with contains the bbox coordinates
+        :return corners: Returns the sorted input array
+    '''
     # first sort upper lower
     index_upper = np.argsort(corners[1])
     corners = corners[..., index_upper]
@@ -180,18 +190,26 @@ def sort_corners(corners):
 def rename_label_files(path_to_labels):
     ''' This function add the file ending .xml to all label files '''
 
+    # Going thru all dog races directories
     for dir in os.listdir(path_to_labels):
         path_to_dog_race_labels = os.path.join(path_to_labels, dir)
 
+        # Renameing all label file with in a dog race directories
         for label_file in os.listdir(path_to_dog_race_labels):
             path_to_image_label = os.path.join(path_to_dog_race_labels,
                                                label_file)
 
+            # If conditon to prevent renamingfiles which already
+            # including a '.xml' ending
             if '.xml' not in path_to_image_label:
                 os.rename(path_to_image_label, path_to_image_label + '.xml')
-            elif '.xml.xml' in path_to_image_label:
-                new_path_to_image_label = path_to_image_label.replace('.xml', '')
-                os.rename(path_to_image_label, new_path_to_image_label + '.xml')
+
+            # This block just fixes double file endings and is because of the
+            # first if conditon out-commented
+
+            #elif '.xml.xml' in path_to_image_label:
+            #    new_path_to_image_label = path_to_image_label.replace('.xml', '')
+            #    os.rename(path_to_image_label, new_path_to_image_label + '.xml')
 
 
 
