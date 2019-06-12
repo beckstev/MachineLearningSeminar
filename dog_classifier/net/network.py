34cf44b18e1a8e054679c9d6d16b4075835fdcdf
beckstev <beckstev@web.de>
2019-06-12 14:45:00 +0200

    Implemented SeminarNN

--- dog_classifier/net/network.py
+++ dog_classifier/net/network.py
@@ -22,3 +22,18 @@ def DogNN():
     model.add(Dense(120, activation='softmax'))
 
     return model
+
+
+def SeminarNN():
+    shape_input = (128, 128, 3)
+
+    model = Sequential()
+    model.add(Conv2D(filters=256, kernel_size=(7, 7), padding='valid', activation='relu', input_shape=shape_input))
+    model.add(Conv2D(filters=128, kernel_size=(5, 5), padding='valid', activation='relu', input_shape=shape_input))
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='valid', activation='relu', input_shape=shape_input))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    #model.add(GlobalMaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(10, activation='softmax'))
+
+    return model
--- dog_classifier/net/test.py
+++ dog_classifier/net/test.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-from network import DogNN
+from network import DogNN, SeminarNN
 
 df_train = pd.read_csv('../../labels/train_labels.csv')
 
-model = DogNN()
+model = SeminarNN()
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
