34bae3adbacf2ff3383a0a2b760cfe87c283b9d9
beckstev <beckstev@web.de>
2019-06-14 15:54:40 +0200

    Created first DogNN with variable input size

--- dog_classifier/net/network.py
+++ dog_classifier/net/network.py
@@ -5,22 +5,17 @@ from keras import backend as K
 
 def DogNN():
     # K.set_image_dim_ordering('th')
-    # shape_input = ( 3, 500, 500)
-    shape_input = (500, 500, 3)
+    shape_input = (None, None, 3)
 
     model = Sequential()
-    model.add(Conv2D(filters=256, kernel_size=(7, 7), activation='relu',
+    model.add(Conv2D(filters=8, kernel_size=(7, 7), activation='relu',
                      input_shape=shape_input))
-
-    model.add(Conv2D(filters=128, kernel_size=(5, 5), activation='relu'))
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(filters=8, kernel_size=(5, 5), activation='relu'))
+    model.add(Conv2D(filters=8, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(GlobalMaxPooling2D())
-    model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(256, activation='relu'))
+    model.add(Dense(120, activation='relu'))
     model.add(Dense(120, activation='softmax'))
-
     return model
 
 
