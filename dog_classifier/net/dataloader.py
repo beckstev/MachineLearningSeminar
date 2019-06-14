8b22b30621cb96b6aed4da90d9bb3de727807d1e
beckstev <beckstev@web.de>
2019-06-14 19:57:02 +0200

    Preapared dataloader.py for pull request (added comments)

--- dog_classifier/net/dataloader.py
+++ dog_classifier/net/dataloader.py
@@ -4,10 +4,24 @@ from keras.utils import to_categorical, Sequence
 
 
 class DataGenerator(Sequence):
+    ''' Dataloader for batch wise traning of a NN with batch wise rescaling of
+        images. The function inherits the properties of keras.utils.Sequence
+        so that we can leverage nice functionalities such as multiprocessing.
+    '''
 
-    def __init__(self, df, batch_size=16, n_channels=3, n_classes=120,
+    def __init__(self, df, batch_size=16, n_classes=120,
                  use_rgb=True, shuffle=True, seed=13):
-        'Initialization'
+        '''Initialization
+        :param df: Dataframe of the dataset (training, validation or testing)
+                   which contains for all images the path to the image, the
+                   label and the width & height of the image
+        :param batch_size:  Size of the batch
+        :param n_classes: Number of classes you wanna classify
+        :param use_rgb: Boolean to indicate if the function should use RGB
+                        or grayscale images
+        :param shuffle: To get a new image order every epoch abs
+        :param seed: Set a seed for numpy random functions
+        '''
         self.df = df
         self.batch_size = batch_size
         self.number_IDs = len(df)
@@ -19,17 +33,30 @@ class DataGenerator(Sequence):
 
     def on_epoch_end(self):
         'Updates indexes after each epoch'
-        self.indexes = np.arange(self.number_IDs)
+        self.data_index = np.arange(self.number_IDs)
+
         if self.shuffle is True:
             np.random.seed(self.seed)
-            np.random.shuffle(self.indexes)
+            np.random.shuffle(self.data_index)
 
     def __data_generation(self, list_IDs_temp):
-        'Generates data containing batch_size samples'
-        # X : (n_samples, *dim, n_channels)
-        # Initialization
-
+        ''' Function which load and rescaled all the images for batch. The
+            images will be scaled to the size of the smallest image in a batch.
+            Although, the NN architecture is image size independent we need
+            to resize images. The reason for this is, that numpy arrays have
+            a fixed shape.
+            :param list_IDs_temp: Indexes of the images which are part of the
+                                  batch
+            :return X: A 4D numpy array (batch_size, min_width, min_height, channels)
+                       containing the rescaled images of the batch.
+            :return y: The labels of the images as 2D matrix
+                       (batch_size, number_of_classes). To create the matrix
+                       the fucntion keras.utils.to_categorical is used.
+        '''
         if self.use_rgb is True:
+            # The function cv2.imread has an argument to read an image in RGB
+            # or grayscale mode. 1 = RGB, 0 = Grayscale. Compare
+            # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_image_display/py_image_display.html
             colormode = 1
             n_channels = 3
         else:
@@ -39,6 +66,9 @@ class DataGenerator(Sequence):
         width_of_batch_images = []
         height_of_batch_images = []
 
+        # We have to get the minmal image width and height of the batch.
+        # Therefore, we loop over the dataframe to get width and height of
+        # every image inside the batch
         for ID in list_IDs_temp:
             # Store sample
             width_of_batch_images.append(int(self.df['width'].values[ID]))
@@ -47,19 +77,21 @@ class DataGenerator(Sequence):
         min_width_of_batch = min(width_of_batch_images)
         min_height_of_batch = min(height_of_batch_images)
 
+        # Tuple which will be used for cv2.rescale
         rescale_size = (min_height_of_batch, min_width_of_batch)
+
+        # This array will be used to save the resized images. As we can see
+        # the size of the array is fixed.
         X = np.empty((self.batch_size, min_width_of_batch, min_height_of_batch,
                      n_channels))
+        # List to save the labels
         y = []
 
         for i, ID in enumerate(list_IDs_temp):
             path_to_image = self.df['path_to_image'].values[ID]
             image = cv.imread(path_to_image, colormode)
-            #plt.imshow(image)
-            #plt.show()
             rescaled_image = cv.resize(image, rescale_size)
-            #plt.imshow(rescaled_image)
-            #plt.show()
+
             X[i, ] = rescaled_image
             y.append(self.df['race_label'].values[ID])
 
@@ -70,12 +102,19 @@ class DataGenerator(Sequence):
         return int(np.floor(self.number_IDs / self.batch_size))
 
     def __getitem__(self, index):
-        'Generate one batch of data'
+        '''Generate one batch of data
+        :return X: A 4D numpy array (batch_size, min_width, min_height, channels)
+                   containing the rescaled images of the batch.
+        :return y: The labels of the images as 2D matrix
+                   (batch_size, number_of_classes). To create the matrix
+                   the fucntion keras.utils.to_categorical is used.
+       '''
+
         # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size:(index+1)*self.batch_size]
+        index_of_dataset = self.data_index[index * self.batch_size:(index+1)*self.batch_size]
 
         # Find list of IDs
-        list_IDs_temp = [self.indexes[k] for k in indexes]
+        list_IDs_temp = [self.data_index[k] for k in index_of_batch_img]
 
         # Generate data
         X, y = self.__data_generation(list_IDs_temp)
