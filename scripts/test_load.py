8ff741d7baf7871bea889cee2b3789dc2a01688e
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-06-16 19:25:16 +0200

    Delete unecessary files

--- scripts/test_load.py
+++
@@ -1,9 +0,0 @@
-from keras.models import load_model
-import os
-
-pfad = "build"
-if not os.path.exists(pfad):
-    os.makedirs(pfad)
-
-model = load_model('build/my_model.h5')
-print(model.summary())
