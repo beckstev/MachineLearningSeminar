000b050f64818e97ef72a00419f9d42d80a7ef27
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-06-16 21:59:25 +0200

    Added new funktions to evaluate_training
    also used argparser for evaluate. To DO: make confusion matrix look
    better.

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,47 +1,35 @@
-import pandas as pd
-import numpy as np
+import argparse
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
-# from keras.models import load_model
 from dog_classifier.evaluate import evaluate_training as eval
-from dog_classifier.net.dataloader import DataGenerator
 from sklearn.metrics import confusion_matrix
-from keras.utils import to_categorical
 
 
-# model = load_model('../saved_models/our_first_nn.h5')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Usage: python evaluate.py  <Path to Prediciton>, <Path to Encoder>, <File Name of Prediction>')
+    parser.add_argument('prediction_path', type=str, help='path to save/saved prediction')
+    parser.add_argument('encoder_path', type=str, help='path to encoder')
+    parser.add_argument('fname_pred', type=str, help='file name of prediction file')
+    parser.add_argument('--model_path', type=str, help='path to saved model')
+    parser.add_argument('--init', type=bool, help='Flag to predict one time.')
 
-df_test = pd.read_csv('../labels/test_labels.csv')
-# df_test = df_test.iloc[:32, :]
-testDataloader = DataGenerator(df_test, shuffle=True)
-# Encoder for labels -> convert strings to binary classes
-encoder = LabelEncoder()
-encoder.fit(df_test['race_label'].values)
-df_test['race_label'] = encoder.transform(df_test['race_label'].values)
-Y_pred = np.genfromtxt('build/prediction.txt')
+    args = parser.parse_args()
+    init = args.init if args.init else False
 
-# Y_test erstellen, indem race_labels konvertiert werden
-values = df_test['race_label'].values
-test = values[testDataloader.data_index]
-Y_test = to_categorical(test[:-4], num_classes=None)
+    if init:
+        eval.predict(args.model_path, args.prediction_path, args.encoder_path,
+                     args.fname_pred)
 
-# print(Y_pred.shape)
-# Convert predictions classes to one hot vectors
-Y_cls = np.argmax(np.array(Y_pred), axis=1)
+    Y_pred, Y_test, Y_cls, Y_true = eval.preprocess(args.prediction_path +
+                                                    args.fname_pred,
+                                                    args.encoder_path)
 
-# Convert validation observations to one hot vectors
-Y_true = np.argmax(np.array(Y_test), axis=1)
+    # Multiclass-Analyse
+    eval.prob_multiclass(Y_pred, Y_test, label=10, path='.')
 
-# Multiclass-Analyse
-eval.prob_multiclass(Y_pred, Y_test, label=10, fname="multiclass")
+    # compute the confusion matrix
+    confusion_mtx = confusion_matrix(Y_true, Y_cls)
 
-# compute the confusion matrix
-confusion_mtx = confusion_matrix(Y_true, Y_cls)
-
-# plot the confusion matrix
-# Prblem with figure size, still need fixing, some axis is cut off all the time
-plt.figure(figsize=(8, 8))
-eval.plot_confusion_matrix(confusion_mtx, classes=range(120), fname='cm_norm')
-# plt.figure(figsize=(8, 8))
-# eval.plot_confusion_matrix(confusion_mtx, classes=range(120),
-                           # normalize=False, fname='cm')
+    # plot the confusion matrix
+    # Prblem with figure size, still need fixing, some axis is cut off
+    # plt.figure(figsize=(8, 8))
+    # eval.plot_confusion_matrix(confusion_mtx, classes=range(120), path=".")
