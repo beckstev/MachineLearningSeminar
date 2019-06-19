34abf66af980ba39e20a442e32c17015b27e52c8
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-06-19 15:02:39 +0200

    argparser changes
    Changed, that all files will be saved in the prediciton path, so that
    its tidy. Also no prediciton name necessary, cause there ist a default
    one. Last but not least, changed to encoder_name

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -4,26 +4,27 @@ from sklearn.metrics import confusion_matrix
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Usage: python evaluate.py  <Path to Prediciton>, <Path to Encoder>, <File Name of Prediction>')
+    parser = argparse.ArgumentParser(description='Usage: python evaluate.py  <Path to Prediciton>, <Path to Encoder>')
     parser.add_argument('prediction_path', type=str, help='path to save/saved prediction')
-    parser.add_argument('encoder_path', type=str, help='path to encoder')
-    parser.add_argument('fname_pred', type=str, help='file name of prediction file')
+    parser.add_argument('encoder_model', type=str, help='encoder model to use')
+    parser.add_argument('--fname_pred', type=str, help='file name of prediction file')
     parser.add_argument('--model_path', type=str, help='path to saved model')
     parser.add_argument('--init', type=bool, help='Flag to predict one time.')
 
     args = parser.parse_args()
     init = args.init if args.init else False
+    fname_pred = args.fname_pred if args.fname_pred else 'prediction.txt'
 
     if init:
-        eval.predict(args.model_path, args.prediction_path, args.encoder_path,
-                     args.fname_pred)
+        eval.predict(args.model_path, args.prediction_path, args.encoder_model,
+                     fname_pred)
 
     Y_pred, Y_test, Y_cls, Y_true = eval.preprocess(args.prediction_path +
-                                                    args.fname_pred,
-                                                    args.encoder_path)
+                                                    fname_pred,
+                                                    args.encoder_model)
 
     # Multiclass-Analyse
-    eval.prob_multiclass(Y_pred, Y_test, label=10, path='.')
+    eval.prob_multiclass(Y_pred, Y_test, label=10, path=args.prediction_path)
 
     # compute the confusion matrix
     confusion_mtx = confusion_matrix(Y_true, Y_cls)
@@ -31,4 +32,4 @@ if __name__ == '__main__':
     # plot the confusion matrix
     # Prblem with figure size, still need fixing, some axis is cut off
     # plt.figure(figsize=(8, 8))
-    eval.plot_confusion_matrix(confusion_mtx, classes=range(120), path=".")
+    eval.plot_confusion_matrix(confusion_mtx, classes=range(120), path=args.prediction_path)
