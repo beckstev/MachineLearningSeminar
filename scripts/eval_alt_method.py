bc497b5373514776019df3d0ce434a2badeb6fbb
beckstev <beckstev@web.de>
2019-06-28 18:23:05 +0200

    Method to visualize random forest results

---
+++ scripts/eval_alt_method.py
@@ -0,0 +1,12 @@
+import argparse
+
+from dog_classifier.evaluate import evaluate_randomforest as eval_rf
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Usage: python eval_alt_method.py <Encoder>')
+    parser.add_argument('encoder_model', type=str, help='encoder model to use')
+    parser.add_argument('image_resize', type=int, nargs=2, help=' Width & Height wich determines the shape of the resized images')
+
+    args = parser.parse_args()
+    img_resize = tuple(args.image_resize) if args.image_resize else None
+    eval_rf.visualize_rf_preduction(args.encoder_model, img_resize)
