17876b3a1877fa92e007723027470ecdba89e62e
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-07-04 16:23:21 +0200

    added installing of required packages
    specified in requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
+# reads in the requirements.txt
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 setup(name='dog_classifier',
+      install_requires=install_requires,
       version='1.0',
       author="Steven Becker, Felix Geyer",
       packages=['dog_classifier'])
