3357371872f5fe25acc8ba6b9a3586f1d0b9f622
beckstev <beckstev@web.de>
2019-07-08 20:43:01 +0200

    modified start parameter for training

--- scripts/hyper_tuning.py
+++ scripts/hyper_tuning.py
@@ -21,15 +21,15 @@ if __name__ == '__main__':
     if args.epochs:
         n_epochs = args.epochs
 
-    learning_rate = [0.1, 1e-3, 1e-5, 1e-6]
+    learning_rate = 0.001
     if args.learning_rate:
         learning_rate = args.learning_rate
 
-    bs_size = [1, 5, 10, 15, 17, 25]
+    bs_size = [2, 5, 10, 15, 17, 25]
     if args.batch_size:
         bs_size = args.batch_size
 
-    l2_reg = [0.01, 0.001, 0.005, 0.001, 0.00001]
+    l2_reg = [0.01, 0.001, 0.005, 0.0001]
     if args.regularisation_rate:
         l2_reg = args.regularisation_rate
 
@@ -41,15 +41,11 @@ if __name__ == '__main__':
     if args.early_stopping_delta:
         early_stopping_delta = args.early_stopping_delta
 
-    if args.use_rgb:
-        norm_mean, norm_std = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225]
-    else:
-        norm_mean, norm_std = [0.5], [0.2]
-
     n_classes = 120
     if args.n_classes:
         n_classes = args.n_classes
     img_resize = args.imgage_resize if args.imgage_resize else None
+    use_rgb = [True, False]
 
     training_parameters = {'n_classes': n_classes,
                            'batch_size': bs_size,
@@ -61,10 +57,7 @@ if __name__ == '__main__':
                            'encoder_model': args.encoder_model,
                            'early_stopping_patience': early_stopping_patience,
                            'early_stopping_delta': early_stopping_delta,
-                           'img_resize': img_resize,
-                           'normalization': {
-                                            'mean': norm_mean,
-                                            'std': norm_std}
+                           'img_resize': img_resize
                            }
 
-    find_parameters(training_parameters, bs_size, learning_rate, l2_reg)
+    find_parameters(training_parameters, bs_size, use_rgb, l2_reg)
