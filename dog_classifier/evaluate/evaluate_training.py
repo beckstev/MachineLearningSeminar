13e56eab3de78492e0e8695d262db1b4cd4f5fa5
Felix Geyer <felix.geyer@tu-dortmund.de>
2019-07-27 14:25:07 +0200

    Neue Methode eingefügt, um von model_hsitory zu plotten

--- dog_classifier/evaluate/evaluate_training.py
+++ dog_classifier/evaluate/evaluate_training.py
@@ -92,6 +92,45 @@ def plot_history(network_history, path):
     plt.clf()
 
 
+def plot_history_special(history, path):
+    """function for plotting the loss and the accuracy from a saved model
+
+    :param history: Dataframe generated from model_history.csv
+    :param path: saving path
+    """
+    print("plot history")
+    path = path + '/build/'
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    num_epochs = len(history['loss'])
+    epoche = np.arange(1, num_epochs + 1)
+
+    fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(5.8, 3.58), sharex=True)
+
+    ax0.set_ylabel('Loss')
+    ax0.plot(epoche, history['loss'], label='Training')
+    ax0.plot(epoche, history['val_loss'], label='Validation')
+
+    ymin, ymax = ax0.get_ylim()
+    y_ticks = np.round(np.linspace(ymin, ymax, 4), 2)
+    ax0.set_yticks(y_ticks)
+    ax0.legend()
+
+    ax1.set_xlabel('Epochs')
+    ax1.set_ylabel('Accuracy')
+    ax1.plot(epoche, history['acc'])
+    ax1.plot(epoche, history['val_acc'])
+
+    ymin, ymax = ax1.get_ylim()
+    y_ticks = np.round(np.linspace(ymin, ymax, 4), 2)
+    ax1.set_yticks(y_ticks)
+    plt.tight_layout()
+    plt.savefig("{}/history.pdf".format(path), dpi=500,
+                pad_inches=0, bbox_inches='tight')
+    plt.clf()
+
+
 def prob_multiclass(Y_pred, Y_test, Y_true, label, path):
     """defines a multiclass probability in the case that the output of the cnn
     cannot be interpreted as a probability. Also plots for a given label
